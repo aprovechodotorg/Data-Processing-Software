5cd44a13474989f6101db19b7e72a44252bc5e4c
kiernan-kilkenny <127541433+kiernan-kilkenny@users.noreply.github.com>
2023-07-10 17:56:34 -0700

    Updated fuel removal function
    Finalized newest version of fuel removal function with help from Jaden, removed the test version of the function and cleaned up the code

--- PEMS/PEMS_FuelDataCleaning.py
+++ PEMS/PEMS_FuelDataCleaning.py
@@ -54,14 +54,14 @@ def dev_plot_fuel_data(raw_data):
     ax2.set_title('Cleaned Data')
 
     # Call to fuel_removal function to generate removal events, quantities, and times
-    kg_rem, time_rem_act, kg_rem_plot = test_fuel_removal(new_data, fuel_data)
-    print(f'Number of fuel removal events using fuel_removal function: {len(kg_rem_plot)}')
+    kg_rem, time_rem, removal_start, removal_end, rem_timestamp, load_freq = fuel_removal(new_data, fuel_data)
+    print(f'Number of fuel removal events using fuel_removal function: {len(removal_start)}')
     print(f'Total mass of fuel removed using fuel_removal function: {round(sum(kg_rem), 2)} kg')
     mass_removed(new_data)
 
     # Plot fuel removal data over cleaned data
     ax3.plot(raw_data['seconds'], new_data)
-    ax3.plot(time_rem_act, kg_rem_plot, 'ro', mfc='none')
+    ax3.plot(time_rem, removal_start, 'ro', mfc='none')
     ax3.set_xlabel('Time (s)')
     ax3.set_ylabel('Fuel Weight (kg)')
     ax3.set_title('Fuel Removal Events')
@@ -82,21 +82,23 @@ def plot_fuel_data(raw_data):
     new_data = alt_clean_fuel_data(raw_data)
 
     # Call to fuel_removal function to generate removal events, quantities, and times
-    kg_rem, time_rem_act, kg_rem_plot = fuel_removal(new_data, fuel_data)
-    print(f'Number of fuel removal events using fuel_removal function: {len(kg_rem_plot)}')
+    kg_rem, time_rem_act, removal_start, removal_end, rem_timestamp, load_freq = fuel_removal(new_data, fuel_data)
+    print(f'Number of fuel removal events using fuel_removal function: {len(removal_start)}')
     print(f'Total mass of fuel removed using fuel_removal function: {round(sum(kg_rem), 2)} kg')
     mass_removed(new_data)
 
     # Ask user to input the household number for the plot title
+    # *** don't need this later - should be able to read in hh number from data entry sheet
     hh = input('Please enter the household number for this data: ')
 
     # Plot fuel removal data over cleaned data
     ax.plot(raw_data['seconds'], new_data)
-    ax.plot(time_rem_act, kg_rem_plot, 'ro', mfc='none')
+    ax.plot(time_rem_act, removal_start, 'ro', mfc='none')
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Fuel Weight (kg)')
     ax.set_title(f'Fuel Removal Events {hh}')
-    plt.show()
+    fig.set_size_inches(10, 5)
+    plt.savefig(f'C:\\Users\\kiern\\Downloads\\GP003\\3.8.23\\{hh}.png')
 
 
 # *** TO DO: Decide whether to use the backwards-looking moving median or the central moving median.
@@ -158,25 +160,27 @@ def alt_clean_fuel_data(raw_data, window_size=30):
     return smooth_data
 
 
-# *** TO DO: Figure out how to find masses for each individual removal event
-#            Add code that finds the time in between removal events and saves these time differences to an array. What
+# *** TO DO: Add code that finds the time in between removal events and saves these time differences to an array. What
 #               should we do with those time differences?
 #            Add code that takes the index of a removal event and gets the timestamp from the raw data. What should we
 #               do with the timestamps once we have them?
-def fuel_removal(smooth_data, raw_data, threshold=0.25):
-    """Locates fuel removal events and saves them to arrays that can be used to calculate the total amount of fuel
-    removed in kg, as well as be used for plotting.
+def fuel_removal(smooth_data, raw_data, threshold=0.25, merge_threshold=15):
+    """Locates fuel removal events and saves them to arrays that can be used to calculate the total mass of fuel
+    removed (kg) and be used for plotting.
 
     :param smooth_data: Array of cleaned fuel data created with clean_fuel_data
     :param raw_data: Raw fuel data dictionary loaded in with PEMS_FuelLoadData, used here for the 'seconds' values
     :param threshold: Threshold value for defining a real fuel removal event, defaults to 0.5 kg
-    :return: kg_removed - Mass of fuel removed in kg;
+    :param merge_threshold: Window size for merging removal events, defaults at 15 (1 minute for a 4-second log rate)
+    :return: kg_removed - Array containing mass values (kg) of fuel removed per event;
              time_removed - Array with times of fuel removal events in seconds;
-             kg_rem_plot - Array with mass values of fuel removal events in kg for plotting"""
+             removal_startpoint - Array with mass values (kg) at the start of fuel removal events for plotting;
+             removal_endpoint - Array with mass values (kg) at the end of fuel removal events for plotting;
+             removal_timestamp - Array with timestamps at the start of fuel removal events;
+             loading_frequency - Array with time (s) between each loading event"""
 
     fuel = smooth_data
     time = raw_data['seconds']
-    kg_removed = []
     removal_event = []
 
     # Loop through each element in the cleaned data from clean_fuel_data
@@ -186,7 +190,6 @@ def fuel_removal(smooth_data, raw_data, threshold=0.25):
             # it is a fuel removal event. Save the weight change (kg of fuel removed), time (both index and value), and
             # label this as a potential removal event
             if (kg - fuel[idx + 1]) > threshold:
-                kg_removed.append(kg - fuel[idx + 1])
                 removal_event.append(1)
             else:
                 removal_event.append(0)
@@ -195,7 +198,6 @@ def fuel_removal(smooth_data, raw_data, threshold=0.25):
     # This next piece of code checks to see if removal events are too close together (e.g., there was only one removal
     # event, but the prior code categorized it as two or three events) and merges them into a single event. Based on
     # the cooking event merge algorithm in Fire Finder
-    merge_threshold = 30
     removal_time = 0
     time_since_no_removal = 0
     removal = False
@@ -226,112 +228,37 @@ def fuel_removal(smooth_data, raw_data, threshold=0.25):
         if rem == 1:
             removal_index.append(idx)
 
-    # Use removal event indices to find when fuel was removed and how much fuel was removed at that time (for plotting)
+    # Use removal event indices to find when fuel was removed, the mass of fuel at that time for plotting, and the
+    # timestamp of that fuel removal event
     time_removed = []
-    kg_rem_plot = []
+    removal_startpoint = []
+    removal_timestamp = []
     for idx in removal_index:
         time_removed.append(time[idx])
-        kg_rem_plot.append(fuel[idx])
-
-    return kg_removed, time_removed, kg_rem_plot
-
-
-# *** I am using this function to test adding the capability to get fuel removal load size as an output
-# *** TO DO: Figure out how to use smooth data instead of raw data and still get accurate load size
-#            See if I can improve how the algorithm finds removal endpoints, this should give more accurate output
-def test_fuel_removal(smooth_data, raw_data, threshold=0.25, window_size=30):
-    """Locates fuel removal events and saves them to arrays that can be used to calculate the total amount of fuel
-    removed in kg, as well as be used for plotting.
-
-    :param smooth_data: Array of cleaned fuel data created with clean_fuel_data
-    :param raw_data: Raw fuel data dictionary loaded in with PEMS_FuelLoadData, used here for the 'seconds' values
-    :param threshold: Threshold value for defining a real fuel removal event, defaults to 0.5 kg
-    :return: kg_removed - Mass of fuel removed in kg;
-             time_removed - Array with times of fuel removal events in seconds;
-             kg_rem_plot - Array with mass values of fuel removal events in kg for plotting"""
-
-    # *** Using raw data instead of smooth data right now, need to fix this
-    fuel = smooth_data
-    time = raw_data['seconds']
-    kg_removed = []
-    removal_event = []
-
-    # Loop through each element in the cleaned data from clean_fuel_data
-    for idx, kg in enumerate(fuel):
-        if idx < len(fuel) - 1:
-            # If the change in fuel weight from this data point to the next is greater than your threshold value,
-            # it is a fuel removal event. Save the weight change (kg of fuel removed), time (both index and value), and
-            # label this as a potential removal event
-            if (kg - fuel[idx + 1]) > threshold:
-                kg_removed.append(kg - fuel[idx + 1])
-                removal_event.append(1)
-            else:
-                removal_event.append(0)
-    removal_event.append(0)
-
-    # This next piece of code checks to see if removal events are too close together (e.g., there was only one removal
-    # event, but the prior code categorized it as two or three events) and merges them into a single event. Based on
-    # the cooking event merge algorithm in Fire Finder
-    merge_threshold = 30
-    removal_time = 0
-    time_since_no_removal = 0
-    removal = False
-
-    # Go through the loop once for each element in the fuel data
-    for i in range(len(fuel)):
-        # If no fuel was removed, increase time since last removal event
-        if removal_event[i] == 0:
-            time_since_no_removal += 1
-            removal = False
-        elif removal_event[i] == 1:
-            # If fuel was removed now but not in the previous data point, check if less time than merge_threshold has
-            # passed and some time has passed since a removal event. If so, remove prior removal events in this window
-            if not removal and time_since_no_removal < merge_threshold and removal_time > 0:
-                for j in range(time_since_no_removal):
-                    removal_event[i - j] = 0
-            # If fuel was removed now and in the previous data point, as well as the other criteria above, remove the
-            # current removal event
-            elif removal and time_since_no_removal < merge_threshold and removal_time > 0:
-                removal_event[i] = 0
-            removal = True
-            time_since_no_removal = 0
-            removal_time += 1
-
-    # Get indices of removal event elements
-    removal_index = []
-    for idx, rem in enumerate(removal_event):
-        if rem == 1:
-            removal_index.append(idx)
-
-    # Use removal event indices to find when fuel was removed and how much fuel was removed at that time (for plotting)
-    time_removed = []
-    kg_rem_plot = []
-    for idx in removal_index:
-        time_removed.append(time[idx])
-        kg_rem_plot.append(fuel[idx])
+        removal_startpoint.append(fuel[idx])
+        removal_timestamp.append((raw_data['time'][idx]))
 
     # Use removal event indices to find endpoints of removal events for calculating load size
     removal_endpoint = []
     for i in removal_index:
         # Starting at the beginning of a removal event, look forward to find where the slope smooths out and save this
         # point as the endpoint of the removal event
-        for j, kg in enumerate(fuel[i:i + floor(window_size/2)]):
+        for j, kg in enumerate(fuel[i:i + merge_threshold - 1]):
             if -0.05 < kg - fuel[i + 3] < 0.05:
                 removal_endpoint.append(kg)
                 break
 
-    print(kg_rem_plot)
-    print(removal_endpoint)
+    # Take the difference between removal start and endpoints to find the mass of fuel removed (kg) per loading event
+    kg_removed = []
+    for i, j in enumerate(removal_startpoint):
+        kg_removed.append(round(j - removal_endpoint[i], 2))
 
-    removal_difference = []
-    for i, j in enumerate(kg_rem_plot):
-        removal_difference.append(round(j - removal_endpoint[i], 2))
+    # Take the difference between each removal start time (s) to find loading frequency
+    loading_frequency = []
+    for i in range(len(time_removed) - 1):
+        loading_frequency.append(time_removed[i + 1] - time_removed[i])
 
-    print(removal_difference)
-    print(sum(removal_difference))
-    print(f'Number of removal endpoints found: {len(removal_endpoint)}')
-
-    return kg_removed, time_removed, kg_rem_plot
+    return kg_removed, time_removed, removal_startpoint, removal_endpoint, removal_timestamp, loading_frequency
 
 
 def mass_removed(smooth_data):
@@ -362,7 +289,7 @@ def mass_removed(smooth_data):
         if kg < 0:
             mass.append(abs(kg))
 
-    print(f'Total mass of fuel removed using mass_removed function: {round(sum(mass), 2)} kg')
+    print(f'Total mass of fuel removed using mass_removed function: {round(sum(mass), 2)} kg (overestimate)')
     return mass
 
 
@@ -370,8 +297,8 @@ if __name__ == "__main__":
     # Run tests for this script here
 
     # Hardcoded input path for testing
-    sheetinputpath = "D:\\School Stuff\\MS Research\\3.14.23\\3.14.23_FuelData.csv"
-    # sheetinputpath = "D:\\School Stuff\\MS Research\\3.21.23\\3.21.23_FuelData.csv"
+    # sheetinputpath = "D:\\School Stuff\\MS Research\\3.14.23\\3.14.23_FuelData.csv"
+    sheetinputpath = "C:\\Users\\kiern\\Downloads\\GP003\\3.8.23\\3.8.23_FuelData.csv"
     directory, filename = os.path.split(sheetinputpath)
     data_directory, testname = os.path.split(directory)
 
@@ -382,7 +309,7 @@ if __name__ == "__main__":
     fuel_data = load_fuel_data(inputpath)
 
     # Clean data and show plots
-    dev_plot_fuel_data(fuel_data)
+    plot_fuel_data(fuel_data)
 
     # # Show final polished plot
     # plot_fuel_data(fuel_data)
