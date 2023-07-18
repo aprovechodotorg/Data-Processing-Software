0ee616c9c1af3c779ad4494ae30fbd034cebf161
kiernan-kilkenny <127541433+kiernan-kilkenny@users.noreply.github.com>
2023-07-18 10:19:39 -0700

    Update PEMS_FuelDataCleaning.py
    Added in ability to check removal events against EXACT data to determine if they are also loading events. This still needs to be improved, I think it is excluding events that are happening as the stove is cooling down, but that may still be loading events

--- PEMS/PEMS_FuelDataCleaning.py
+++ PEMS/PEMS_FuelDataCleaning.py
@@ -21,13 +21,12 @@ import csv
 import re
 import matplotlib.pyplot as plt
 import matplotlib
-from datetime import datetime as dt
 from datetime import datetime, timedelta
 import os
 from PEMS_FuelLoadData import load_fuel_data
 from PEMS_FuelLoadData import load_exact_data
 import numpy as np
-from statistics import median
+from statistics import mean, median
 from math import floor
 
 
@@ -56,8 +55,10 @@ def dev_plot_fuel_data(raw_fuel_data, raw_exact_data):
     ax2.set_title('Cleaned Data')
 
     # Call to fuel_removal function to generate removal events, quantities, and times
-    kg_rem, time_rem, removal_start, removal_end, rem_timestamp, load_freq = fuel_removal(new_data, fuel_data,
-                                                                                          exact_data)
+    # kg_rem, time_rem, removal_start, removal_end, rem_timestamp, load_freq = fuel_removal(new_data, fuel_data)
+    kg_rem, time_rem, removal_start, removal_end, rem_timestamp, load_freq = fuel_removal_with_exact(new_data,
+                                                                                                     fuel_data,
+                                                                                                     exact_data)
     print(f'Number of fuel removal events using fuel_removal function: {len(removal_start)}')
     print(f'Total mass of fuel removed using fuel_removal function: {round(sum(kg_rem), 2)} kg')
     mass_removed(new_data)
@@ -70,10 +71,13 @@ def dev_plot_fuel_data(raw_fuel_data, raw_exact_data):
     ax3.set_title('Fuel Removal Events')
 
     # Plot EXACT data
-    ax4.plot(raw_exact_data['seconds'], raw_exact_data['Temperature'], 'tab:orange')
+    ax4.plot(raw_exact_data['seconds'][:len(raw_exact_data['Temperature'])], raw_exact_data['Temperature'],
+             'tab:orange')
     ax4.set_xlabel('Time (s)')
     ax4.set_ylabel('Temperature (C)')
     ax4.set_title('Stove Temperature (gage)')
+    if raw_exact_data['time'][1] - raw_exact_data['time'][0] == timedelta(seconds=4):
+        ax4.set_xlim(right=raw_fuel_data['seconds'][-1])
     fig.subplots_adjust(hspace=0.65)
     fig.set_size_inches(6.4, 11)
     plt.show()
@@ -171,13 +175,12 @@ def alt_clean_fuel_data(raw_fuel_data, window_size=30):
 
 
 # *** TO DO: Use EXACT data to determine whether removal events represent fuel being loaded into the fire
-def fuel_removal(smooth_fuel_data, raw_fuel_data, raw_exact_data, threshold=0.25, merge_threshold=15):
+def fuel_removal(smooth_fuel_data, raw_fuel_data, threshold=0.25, merge_threshold=15):
     """Locates fuel removal events and saves them to arrays that can be used to calculate the total mass of fuel
     removed (kg) and be used for plotting.
 
     :param smooth_fuel_data: Array of cleaned FUEL data created with clean_fuel_data
-    :param raw_fuel_data: Raw FUEL data dictionary loaded in with PEMS_FuelLoadData, used here for the 'seconds' values
-    :param raw_exact_data: Raw EXACT data dictionary loaded in with PEMS_FuelLoadData
+    :param raw_fuel_data: Raw FUEL data dictionary loaded in with PEMS_FuelLoadData
     :param threshold: Threshold value for defining a real fuel removal event, defaults to 0.5 kg
     :param merge_threshold: Window size for merging removal events, defaults at 15 (1 minute for a 4-second log rate)
     :return: kg_removed - Array containing mass values (kg) of fuel removed per event;
@@ -190,51 +193,175 @@ def fuel_removal(smooth_fuel_data, raw_fuel_data, raw_exact_data, threshold=0.25
     fuel = smooth_fuel_data
     fuel_time = raw_fuel_data['seconds']
     removal_event = []
-    exact = []
-    exact_time = []
-
-    # Get usage data from EXACT dictionary
-    raw_exact_sorted = sorted(raw_exact_data.keys())
-    raw_exact_usage = raw_exact_data[raw_exact_sorted[0]]
-    exact_usage = []
-
-    # If the EXACT log rate was 1 minute, copy each data point 15 times to make the data usable with the FUEL data
-    if len(raw_exact_data['seconds']) <= len(fuel_time)/4:
-        for temp in raw_exact_data['Temperature']:
-            for i in range(15):
-                exact.append(temp)
-        for use in raw_exact_usage:
-            for j in range(15):
-                exact_usage.append(use)
-        for sec in raw_exact_data['seconds']:
-            exact_time.append(sec)
-            for k in range(14):
-                sec += 4
-                exact_time.append(sec)
-
-    # Now check to see if the EXACT data arrays are the same length as the FUEL data arrays. If not, add to the arrays
-    # to make them the same length as the FUEL arrays
-    if len(exact) < len(fuel):
-        for i in range(len(fuel) - len(exact)):
-            exact.append(exact[-1])
-    if len(exact_usage) < len(fuel):
-        for j in range(len(fuel) - len(exact_usage)):
-            exact_usage.append(exact_usage[-1])
-    if len(exact_time) < len(fuel_time):
-        for k in range(len(fuel_time) - len(exact_time)):
-            exact_time.append(exact_time[-1] + 4)
 
     # First pass: Loop through each element in the cleaned data from clean_fuel_data
     for idx, kg in enumerate(fuel):
         if idx < len(fuel) - 1:
-            # If the change in fuel weight from this data point to the next is greater than your threshold value,
-            # it is a fuel removal event. Save the weight change (kg of fuel removed), time (both index and value), and
-            # label this as a potential removal event
+            # If the change in fuel weight from this data point to the next is greater than your threshold value, this
+            # is the start of a potential removal event
             if (kg - fuel[idx + 1]) > threshold:
                 removal_event.append(1)
             else:
                 removal_event.append(0)
     removal_event.append(0)
+
+    # Second pass: This next piece of code checks to see if removal events are too close together (e.g., there was only
+    # one removal event, but the prior code categorized it as two or three events) and merges them into a single event.
+    # Based on the cooking event merge algorithm in Fire Finder
+    removal_time = 0
+    time_since_no_removal = 0
+    removal = False
+
+    # Go through the loop once for each element in the FUEL data
+    for i in range(len(fuel)):
+        # If no fuel was removed, increase time since last removal event
+        if removal_event[i] == 0:
+            time_since_no_removal += 1
+            removal = False
+        elif removal_event[i] == 1:
+            # If fuel was removed now but not in the previous data point, check if less time than merge_threshold has
+            # passed and some time has passed since a removal event. If so, remove prior removal events in this window
+            if not removal and time_since_no_removal < merge_threshold and removal_time > 0:
+                for j in range(time_since_no_removal):
+                    removal_event[i - j] = 0
+            # If fuel was removed now and in the previous data point, as well as the other criteria above, remove the
+            # current removal event
+            elif removal and time_since_no_removal < merge_threshold and removal_time > 0:
+                removal_event[i] = 0
+            removal = True
+            time_since_no_removal = 0
+            removal_time += 1
+
+    # Get indices of removal event elements
+    removal_index = []
+    for idx, rem in enumerate(removal_event):
+        if rem == 1:
+            removal_index.append(idx)
+
+    # Use removal event indices to find when fuel was removed, the mass of fuel at that time for plotting, and the
+    # timestamp of that fuel removal event
+    time_removed = []
+    removal_startpoint = []
+    removal_timestamp = []
+    for idx in removal_index:
+        time_removed.append(fuel_time[idx])
+        removal_startpoint.append(fuel[idx])
+        removal_timestamp.append((raw_fuel_data['time'][idx]))
+
+    # Use removal event indices to find endpoints of removal events for calculating load size
+    removal_endpoint = []
+    for i in removal_index:
+        # Starting at the beginning of a removal event, look forward to find where the slope smooths out and save this
+        # point as the endpoint of the removal event
+        for j, kg in enumerate(fuel[i:i + merge_threshold - 1]):
+            if -0.05 < kg - fuel[i + 3] < 0.05:
+                removal_endpoint.append(kg)
+                break
+
+    # Take the difference between removal start and endpoints to find the mass of fuel removed (kg) per loading event
+    kg_removed = []
+    for i, j in enumerate(removal_startpoint):
+        kg_removed.append(round(j - removal_endpoint[i], 2))
+
+    # Take the difference between each removal start time (s) to find loading frequency
+    loading_frequency = []
+    for i in range(len(time_removed) - 1):
+        loading_frequency.append(time_removed[i + 1] - time_removed[i])
+
+    return kg_removed, time_removed, removal_startpoint, removal_endpoint, removal_timestamp, loading_frequency
+
+
+# I am using fuel_removal_with_exact to test out verifying fuel loading events against the EXACT data. The fuel_removal
+# function works well on its own but only takes into account the FUEL data when determining loading events
+def fuel_removal_with_exact(smooth_fuel_data, raw_fuel_data, raw_exact_data, threshold=0.25, merge_threshold=15):
+    """Locates fuel removal events and saves them to arrays that can be used to calculate the total mass of fuel
+    removed (kg) and be used for plotting.
+
+    :param smooth_fuel_data: Array of cleaned FUEL data created with clean_fuel_data
+    :param raw_fuel_data: Raw FUEL data dictionary loaded in with PEMS_FuelLoadData
+    :param raw_exact_data: Raw EXACT data dictionary loaded in with PEMS_FuelLoadData
+    :param threshold: Threshold value for defining a real fuel removal event, defaults to 0.5 kg
+    :param merge_threshold: Window size for merging removal events, defaults at 15 (1 minute for a 4-second log rate)
+    :return: kg_removed - Array containing mass values (kg) of fuel removed per event;
+             time_removed - Array with times of fuel removal events in seconds;
+             removal_startpoint - Array with mass values (kg) at the start of fuel removal events for plotting;
+             removal_endpoint - Array with mass values (kg) at the end of fuel removal events for plotting;
+             removal_timestamp - Array with timestamps at the start of fuel removal events;
+             loading_frequency - Array with time (s) between each loading event"""
+
+    fuel = smooth_fuel_data
+    fuel_time = raw_fuel_data['seconds']
+    fuel_timestamps = raw_fuel_data['time']
+    removal_event = []
+    exact = raw_exact_data['Temperature']
+    exact_timestamps = raw_exact_data['time']
+
+    # First pass: Loop through each element in the cleaned data from clean_fuel_data
+    for idx, kg in enumerate(fuel):
+        if idx < len(fuel) - 1:
+            # If the change in fuel weight from this data point to the next is greater than your threshold value, this
+            # is the start of a potential removal event
+            if (kg - fuel[idx + 1]) > threshold:
+                # If the EXACT log rate is 1 minute
+                if exact_timestamps[1] - exact_timestamps[0] == timedelta(minutes=1):
+                    for i, ts in enumerate(exact_timestamps):
+                        # Because the EXACT log rate is 1 minute and the FUEL log rate is 4 seconds, the timestamps will
+                        # not line up. Find an EXACT data point collected within 28 seconds of a FUEL data point. Look
+                        # at the data 20 minutes before and after that point. If the mean temperature after the current
+                        # point is above the mean previous, fuel was loaded into the stove.
+                        if abs(ts - fuel_timestamps[idx]) < timedelta(seconds=32):
+                            temp_prev = []
+                            temp_post = []
+                            if i >= 300 or len(fuel_timestamps) - 1 <= 300:
+                                for temp in exact[i - 300:i]:
+                                    temp_prev.append(temp)
+                                for temp in exact[i:i + 300]:
+                                    temp_post.append(temp)
+                            elif i < 300:
+                                for temp in exact[0:i]:
+                                    temp_prev.append(temp)
+                                for temp in exact[i:i + 300]:
+                                    temp_post.append(temp)
+                            elif len(fuel_timestamps) - i < 300:
+                                for temp in exact[i - 300:i]:
+                                    temp_prev.append(temp)
+                                for temp in exact[i:-1]:
+                                    temp_post.append(temp)
+                            if mean(temp_post) > mean(temp_prev):
+                                removal_event.append(1)
+                            else:
+                                removal_event.append(0)
+                # If the EXACT log rate is 4 seconds
+                elif exact_timestamps[1] - exact_timestamps[0] == timedelta(seconds=4):
+                    for i, ts in enumerate(exact_timestamps):
+                        # If the EXACT and FUEL sensors have the same log rate, find data points with the same timestamp
+                        if ts == fuel_timestamps[idx]:
+                            temp_prev = []
+                            temp_post = []
+                            if i >= 300 >= len(fuel_timestamps) - i:
+                                for temp in exact[i - 300:i]:
+                                    temp_prev.append(temp)
+                                for temp in exact[i:i + 300]:
+                                    temp_post.append(temp)
+                            else:
+                                for temp in exact[0:i]:
+                                    temp_prev.append(temp)
+                                for temp in exact[i:-1]:
+                                    temp_post.append(temp)
+                            if mean(temp_post) > mean(temp_prev):
+                                removal_event.append(1)
+                            else:
+                                removal_event.append(0)
+            else:
+                removal_event.append(0)
+
+    # If the EXACT data log rate is less than that of the FUEL data, need to pad the end of the removal event array
+    if len(fuel) - len(removal_event) > 0:
+        for i in range(len(fuel) - len(removal_event)):
+            removal_event.append(0)
+    else:
+        removal_event.append(0)
+    print(len(fuel), len(removal_event), sum(removal_event))
 
     # Second pass: This next piece of code checks to see if removal events are too close together (e.g., there was only
     # one removal event, but the prior code categorized it as two or three events) and merges them into a single event.
@@ -338,10 +465,12 @@ if __name__ == "__main__":
     # Run tests for this script here
 
     # Hardcoded input path for testing
-    sheetinputpath = "D:\\School Stuff\\MS Research\\3.14.23\\3.14.23_FuelData.csv"
-    # sheetinputpath = "C:\\Users\\kiern\\Downloads\\GP001\\3.16.23\\3.16.23_FuelData.csv"
-    # sheetinputpath = "C:\\Users\\kiern\\Downloads\\GP003\\2.15.23\\2.15.23_FuelData.csv"
-    # sheetinputpath = "C:\\Users\\kiern\\Downloads\\GP003\\3.8.23\\3.8.23_FuelData.csv"
+    sheetinputpath = "D:\\School Stuff\\MS Research\\Data\\GP004\\3.14.23\\3.14.23_FuelData.csv"
+    # sheetinputpath = "D:\\School Stuff\\MS Research\\Data\\GP001\\3.16.23_FuelData.csv"
+    # sheetinputpath = "D:\\School Stuff\\MS Research\\Data\\GP003\\2.15.23\\2.15.23_FuelData.csv"
+    # sheetinputpath = "D:\\School Stuff\\MS Research\\Data\\GP003\\3.8.23\\3.8.23_FuelData.csv"
+    # sheetinputpath = "D:\\School Stuff\\MS Research\\Data\\GP007\\3.15.23\\3.15.23_FuelData.csv"
+    # sheetinputpath = "D:\\School Stuff\\MS Research\\Data\\GP010\\3.16.23\\3.16.23_FuelData.csv"
     directory, filename = os.path.split(sheetinputpath)
     data_directory, testname = os.path.split(directory)
 
@@ -353,9 +482,9 @@ if __name__ == "__main__":
     # Get FUEL and EXACT data
     fuel_data = load_fuel_data(inputpath)
     exact_data = load_exact_data(exactpath)
-    if len(exact_data['Temperature']) < len(exact_data['seconds']):
-        exact_data['seconds'] = exact_data['seconds'][:len(exact_data['Temperature'])]
-    print(f"FUEL: {len(fuel_data['seconds'])} EXACT: {len(exact_data['seconds'])}")
+    # if len(exact_data['Temperature']) < len(exact_data['seconds']):
+    #     exact_data['seconds'] = exact_data['seconds'][:len(exact_data['Temperature'])]
+    # print(f"FUEL: {len(fuel_data['seconds'])} EXACT: {len(exact_data['seconds'])}")
 
     # Clean data and show plots
     dev_plot_fuel_data(fuel_data, exact_data)
