1ad577a26827e831420b4de0ec5b5e1299533ff3
jaden-berger <jaden@aprovecho.org>
2023-10-02 10:08:21 -0700

    Added test data and added testing for L2 and L3. All tests using test data currently passes.

--- test/UCET_unittest.py
+++ test/UCET_unittest.py
@@ -25,11 +25,13 @@ import unittest
 import csv
 from pathlib import Path
 from LEMS_MakeInputFile_EnergyCalcs import LEMS_MakeInputFile_EnergyCalcs
+from PEMS_L2 import PEMS_L2
 from UCET_EnergyCalcs import UCET_EnergyCalcs
+from LEMS_FormatData_L3 import LEMS_FormatData_L3
 
 class UCETunittest(unittest.TestCase):
     def test_energycalcs(self):
-        base_path = Path("./test/GyapaC1T1")
+        base_path = Path("./test/UCET/Gyapa/GyapaC1T1")
         known_data_entry_path = base_path / "GyapaC1T1_DataEntrySheet.xlsx"
 
         calculated_energy_inputs_path = base_path / "GyapaC1T1_EnergyInputs_test.csv"
@@ -86,6 +88,90 @@ class UCETunittest(unittest.TestCase):
         calculated_file.close()
         known_file.close()
 
-        # Compare calculated vs known energy outputs
+        pass
+    def test_L2calcs(self):
+        base_path = Path("./test/UCET/Gyapa")
+        known_energyoutput_path = []
+        known_energyoutput_path.append(base_path / "GyapaC1T1/GyapaC1T1_EnergyOutputs.csv")
+        known_energyoutput_path.append(base_path / "GyapaC1T2/GyapaC1T2_EnergyOutputs.csv")
+        known_energyoutput_path.append(base_path / "GyapaC1T3/GyapaC1T3_EnergyOutputs.csv")
+        known_energyoutput_path.append(base_path / "GyapaC2T1/GyapaC2T1_EnergyOutputs.csv")
+        known_energyoutput_path.append(base_path / "GyapaC2T2/GyapaC2T2_EnergyOutputs.csv")
+        known_energyoutput_path.append(base_path / "GyapaC2T3/GyapaC2T3_EnergyOutputs.csv")
+
+        known_emissionoutput_path = ['N/A','N/A','N/A','N/A','N/A','N/A']
+
+        list_testname = ['GyapaC1T1', 'GyapaC1T2', 'GyapaC1T3', 'GyapaC2T1', 'GyapaC2T2', 'GyapaC2T3',]
+
+        known_L2_output_path = base_path/ "FormattedDataL2.csv"
+
+        calculated_L2_outputpath = base_path/ "FormattedDataL2_test.csv"
+
+        PEMS_L2(known_energyoutput_path, known_emissionoutput_path, calculated_L2_outputpath, list_testname)
+
+        # Compare calculated vs known energy inputs files
+        calculated_file = open(calculated_L2_outputpath, "r")
+        known_file = open(known_L2_output_path, "r")
+
+        known_linecount = 0
+        calculated_linecount = 0
+
+        for calculated_line in calculated_file:
+            calculated_linecount += 1
+        for known_line in known_file:
+            known_linecount += 1
+
+        self.assertEqual(calculated_linecount, known_linecount,
+                         f"Known inputs file line count ({known_linecount}) does not match calculated inputs file line count ({calculated_linecount}) for Level 2 Outputs")
+
+        for calculated_line, known_line in zip(calculated_file, known_file):
+            self.assertEqual(calculated_line, known_line)
+
+        calculated_file.close()
+        known_file.close()
 
         pass
+
+    def test_L3calcs(self):
+        base_path = Path("./test/UCET")
+
+        known_L2_path = []
+        known_L2_path.append(base_path / "Gyapa/FormattedDataL2.csv")
+        known_L2_path.append(base_path / "Traditional/FormattedDataL2.csv")
+        known_L2_path.append(base_path / "TraditionalAluminum/FormattedDataL2.csv")
+
+        for n, val in enumerate(known_L2_path):
+            known_L2_path[n] = str(known_L2_path[n].absolute())
+
+        known_L2_output_path = base_path / "FormattedDataL3.csv"
+
+        calculated_L2_outputpath = base_path / "FormattedDataL3_test.csv"
+
+        log_path = base_path / "UCET_log.txt"
+
+        LEMS_FormatData_L3(known_L2_path, str(calculated_L2_outputpath.absolute()), str(log_path.absolute()))
+
+        # Compare calculated vs known energy inputs files
+        calculated_file = open(calculated_L2_outputpath, "r")
+        known_file = open(known_L2_output_path, "r")
+
+        known_linecount = 0
+        calculated_linecount = 0
+
+        for calculated_line in calculated_file:
+            calculated_linecount += 1
+        for known_line in known_file:
+            known_linecount += 1
+
+        self.assertEqual(calculated_linecount, known_linecount,
+                         f"Known inputs file line count ({known_linecount}) does not match calculated inputs file line count ({calculated_linecount}) for Level 3 Outputs")
+
+        for calculated_line, known_line in zip(calculated_file, known_file):
+            self.assertEqual(calculated_line, known_line)
+
+        calculated_file.close()
+        known_file.close()
+
+        pass
+
+
