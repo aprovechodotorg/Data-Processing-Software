d981a696667161854c515d6358219acee2dffc05
sameb112 <sameb112@gmail.com>
2024-02-07 16:21:41 -0800

    took out full phase from emissions calcs

--- LEMS/LEMS_EmissionCalcs.py
+++ LEMS/LEMS_EmissionCalcs.py
@@ -79,7 +79,7 @@ def LEMS_EmissionCalcs(inputpath,energypath,gravinputpath,aveinputpath,emisoutpu
     
     emissions=['CO','CO2', 'CO2v','PM','VOC']     #emission species that will get metric calculations
 
-    phases=['hp','mp','lp', 'full']
+    phases=['hp','mp','lp']
 
     #Tstd=float(293)     #define standard temperature in Kelvin
     #Pstd=float(101325)   #define standard pressure in Pascals
@@ -613,7 +613,6 @@ def LEMS_EmissionCalcs(inputpath,energypath,gravinputpath,aveinputpath,emisoutpu
             ###################################################
             # carbon in
 
-            phases.remove('full')
             name = 'carbon_in_' + phase
             metricnames.append(name)
             metricunits[name] = 'g'
