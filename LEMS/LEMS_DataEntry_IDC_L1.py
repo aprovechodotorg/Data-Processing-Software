5562813c6163111d9303ae0ca02d65fc5a883218
jaden-berger <jaden@aprovecho.org>
2024-11-21 11:14:25 -0800

    Adding 4007, propagating bc changes through all  scripts. Adding accepted boxes to all messaging

--- LEMS/LEMS_DataEntry_IDC_L1.py
+++ LEMS/LEMS_DataEntry_IDC_L1.py
@@ -1682,11 +1682,12 @@ class LEMSDataInput(tk.Frame):
             self.sensor_path = os.path.join(self.folder_path, f"{os.path.basename(self.folder_path)}_SensorboxVersion.csv")
             self.emissioninputpath = os.path.join(self.folder_path,
                                             f"{os.path.basename(self.folder_path)}_EmissionInputs.csv")
+            self.bcoutputpath = os.path.join(self.folder_path, f"{os.path.basename(self.folder_path)}_BCOutputs.csv")
             logs, data, units = LEMS_EmissionCalcs(self.input_path, self.energy_path, self.grav_path, self.average_path,
                                                    self.output_path, self.all_path, self.log_path, self.phase_path, self.sensor_path,
                                                    self.fuel_path, self.fuelmetric_path, self.exact_path,
                                                    self.scale_path, self.nano_path, self.teom_path, self.senserion_path,
-                                                   self.ops_path, self.pico_path, self.emissioninputpath, self.inputmethod)
+                                                   self.ops_path, self.pico_path, self.emissioninputpath, self.inputmethod, self.bcoutputpath)
             self.emission_button.config(bg="lightgreen")
         except PermissionError:
             message = f"One of the following files: {self.output_path}, {self.all_path} is open in another program. Please close and try again."
@@ -1859,11 +1860,24 @@ class LEMSDataInput(tk.Frame):
             message = f'Something went wrong in Firmware calculations. \n' \
                       f'Please verify that the entered firmware version corresponds to the sensor box number.\n' \
                       f'Accepted firmware versions:\n' \
-                      f'    *SB4003.16\n' \
+                      f'    *SB4002\n' \
+                      f'    *SB4003\n' \
+                      f'    *SB4005\n' \
+                      f'    *SB4007\n' \
+                      f'    *SB4008\n' \
+                      f'    *SB2041\n' \
                       f'    *SB3001\n' \
                       f'    *SB3002\n' \
+                      f'    *SB3009\n' \
+                      f'    *SB3015\n' \
+                      f'    *SB3016\n' \
+                      f'    *Possum2\n' \
                       f'If your sensor box firmware is not one of the ones listed, it can be entered but nothing will be recalibrated.\n' \
                       f'This may lead to issues later.'
+            SB4002, SB4003, SB4005, SB4007, SB4008, SB2041, SB3001, ' \
+                             '
+            SB3002, SB3009, SB3015, SB3016, Possum2
+
             messagebox.showerror("Error", message)
             self.cali_button.config(bg="red")
         except PermissionError:
