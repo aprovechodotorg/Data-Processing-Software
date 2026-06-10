adfec2b672fc754220c010e2be4bfd2a19878795
Travis Volpe <travis@aprovecho.org>
2026-06-10 13:06:44 -0700

    v3.0 bug fixes
    fixed bug that caused blank lines in raw data logs.  tweaked UI to fix header alignment issues and cut-off text.

--- MusaPlotter_3.0/pics_rc.py
+++ MusaPlotter_3.0/pics_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.12.8)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.2)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -206,7 +206,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x5e\x7c\x01\x2f\x30\
+\x00\x00\x01\x9b\x96\xd1\xf9\xb0\
 "
 
 qt_version = [int(v) for v in QtCore.qVersion().split('.')]
